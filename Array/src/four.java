44ed2d43faf7f86680dcdbbba4926f65f1cf9c62
DongDong107 <jkds989@gmail.com>
2022-05-19 22:41:27 +0900

    Update Array Code (20220519)

--- Array/src/four.java
+++ Array/src/four.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class four {
@@ -7,47 +8,21 @@ public class four {
 
 		Scanner sc = new Scanner(System.in);
 
-		int[] arr = new int[10];
+//		int[] arr = new int[10];
+//
+//		for (int i = 0; i < arr.length; i++) {
+//			arr[i] = sc.nextInt() % 42;
+//		}
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt() % 42;
-		}
-
-		int result = 10;
-		int sum = 0;
-		int count = 0;
-		
-		for (int j = 0; j < arr.length; j++) {
-			
-
-			for (int k = 0; k < arr.length; k++) {
-				if (arr[j] == arr[k]) {
-					count++;
-				}
-			}
-			
-			if(count>1) {
-				result = result - count + 1;
-				count = 0;
-			}
-			else
-				count = 0;
-			
-			
-//			sum = sum - count + 1;
-		}
-
-		System.out.print(result-count);
-
+		 HashSet<Integer> set = new HashSet<Integer>();
+		 
+		 for(int i = 0; i < 10; i++) {
+			 
+			 set.add(sc.nextInt()%42);
+		 }
+		 
+		 System.out.print(set.size());
+		 
 	}
 
 }
-
-//같은수의 갯수 N을 카운팅, (-N + 1) 한값들을 더한다.
-
-// result 값은 1 to 10 다 같거나 다 다르거나
-// 만약 같은 숫자들이 나왓어 
-// 1이 3개고  2가 2개     -2 -1       7 나머지 7개 + 1 8 
-// 그 값들만 모아서 마지막ㅇ 뺀다
-// 뭔가 엄청 돌아가는 기분
-// 종류별로 묶으려면 
