b130ea3946e73da193e7c87027ef0bbbeb1ed8c8
DongDong107 <jkds989@gmail.com>
2022-07-29 19:22:48 +0900

    Add 20220729 study

--- BOJ/기본수학/src/기본수학/no1712.java
+++ BOJ/기본수학/src/기본수학/no1712.java
@@ -11,38 +11,49 @@ public class no1712 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		int D = 1;
-		
-		while(true) {
-			
-			if(A+B*D < C*D) {
-				bw.write(String.valueOf(D));
-				break;
-			}
-			else if(B>C) {
-				bw.write(String.valueOf("-1"));
-				break;
-			}
-			else if(A>=2100000000) {
-				bw.write(String.valueOf("2100000001"));
-			}
-			else
-				D++;
+
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+
+		long D = 1;
+		long Q;
+
+		if (C - B == 0) {
+			bw.write(String.valueOf("-1"));
+		} else if (A / (C - B) > 0) {             // >=0 으로 수정해서 맞았다. 이유는?
+			bw.write(String.valueOf(A/(C-B) + 1));
+		} else if (A / (C - B) < 0) {
+			bw.write(String.valueOf("-1"));
 		}
-		
+
+//		if (Q > 0) {
+//			bw.write(String.valueOf(Q + 1));
+//		} else if (Q < 0) {
+//			bw.write(String.valueOf("-1"));
+//		}
+//		while(true) {
+//			
+//			if(A+B*D < C*D) {
+//				bw.write(String.valueOf(D));
+//				break;
+//			}
+//			else if(B>C) {
+//				bw.write(String.valueOf("-1"));
+//				break;
+//			}
+//			else
+//				D++;
+//		}
+
 		bw.flush();
 		bw.close();
-		
+
 	}
 
 }
